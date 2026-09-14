2c0fa683aa34df35afe02adb75ed9ff0df71be30
unknown <lightkapita0@gmail.com>
2026-09-14 14:51:22 +0200

    calculatrice bas à utiliser

--- script.js
+++
@@ -1,5 +0,0 @@
-// Ce fichier est chargé comme un module JavaScript depuis index.html.
-// La logique de calcul et les événements des boutons pourront être ajoutés ici.
-
-// Cette ligne est un exemple de message d'accueil désactivé.
-// alert("Hello, welcome to my website!");
